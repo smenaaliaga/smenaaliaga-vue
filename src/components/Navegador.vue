9797f0c70934124e4fea8f3e728bb30eb41de4f6
Seba <smenaaliaga@gmail.com>
2022-01-31 01:19:31 -0300

    Add files

---
+++ src/components/Navegador.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="nav-desk">
+        <center>
+            <div class="nav-items">
+                <ul>
+                    <li><span><a style="text-decoration: line-through;" href="index.html">Home</a></span></li>
+                    <li><span><a href="projects.html">Projects</a></span></li>
+                    <li><span><a href="assets/CV/CV-Sebastián-Mena.pdf">My CV</a></span></li>
+                    <li><span><a href="contact.html">Contact</a></span></li>
+                </ul>
+            </div>
+        </center>
+    </nav>
+</template>
